6aad60b5500e9d40d90ceedad08dd8b53dd4d861
botd0tnet <74009258+botd0tnet@users.noreply.github.com>
2021-09-01 01:08:57 +0000

    Update delegate.sh

--- delegate.sh
+++ delegate.sh
@@ -6,6 +6,7 @@ CHAINID="umee-betanet-1"
 RATE="uumee"
 ADD_WALLET=$(umeed keys show $WALLET --address)
 VAL_WALLET=$(umeed keys show $WALLET --bech val --address)
+BALANCE=$(umeed query bank balances $(umeed keys show $WALLET --address) | grep "amount" | cut -d'"' -f 2)
 
 umeed tx distribution withdraw-rewards $VAL_WALLET --commission --yes --gas auto --fees 1000${RATE} --from $WALLET --chain-id $CHAINID -y
 
