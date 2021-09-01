b22e1a478b9bb18976c4487871e21084a937d217
botd0tnet <74009258+botd0tnet@users.noreply.github.com>
2021-09-01 01:25:23 +0000

    Update delegate.sh

--- delegate.sh
+++ delegate.sh
@@ -7,15 +7,16 @@ RATE="uumee"
 ADD_WALLET=$(umeed keys show $WALLET --address)
 VAL_WALLET=$(umeed keys show $WALLET --bech val --address)
 BALANCE=$(umeed query bank balances $(umeed keys show $WALLET --address) | grep "amount" | cut -d'"' -f 2)
+BALANCE=$((BALANCE - 1000000))
 
 umeed tx distribution withdraw-rewards $VAL_WALLET --commission --yes --gas auto --fees 1000${RATE} --from $WALLET --chain-id $CHAINID -y
 
 sleep 2
 
 
-umeed tx staking redelegate $VAL_WALLET $VAL_WALLET 2000000${RATE} --gas auto --fees 1000${RATE} --from $WALLET --chain-id $CHAINID -y
+umeed tx staking redelegate $VAL_WALLET $VAL_WALLET $BALANCE${RATE} --gas auto --fees 1000${RATE} --from $WALLET --chain-id $CHAINID -y
 
-
+sleep 2
 
 
 
