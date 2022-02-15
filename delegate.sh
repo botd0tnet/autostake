fe55db6b3db05a45e019c01f25f7bd6e3fb0135b
botd0tnet <74009258+botd0tnet@users.noreply.github.com>
2022-02-15 21:34:47 +0700

    Update delegate.sh

--- delegate.sh
+++ delegate.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 
-WALLET="BotDotNet"
-CHAINID="umee-alpha-mainnet-3"
+WALLET="main"
+CHAINID="umee-1"
 RATE="uumee"
-ADD_WALLET=umeevaloper1sg2vam988aqenkrkdk3er0huuzyupejz8eaam9
-VAL_WALLET=umeevaloper1unmdj75ku4yep237x246dymwx5f2lfw5lwexdt
+ADD_WALLET=umee13g6lzq6mfa8l86lgnsnxacltsssel6h0dg2r55
+VAL_WALLET=umeevaloper13g6lzq6mfa8l86lgnsnxacltsssel6h0dvdv97
 
 
 for(( i=1; i <=9999999; i++ ))
 do
-  echo xxx | umeed tx distribution withdraw-rewards $VAL_WALLET --commission --yes --gas auto --from $WALLET --chain-id umee-alpha-mainnet-3 -y
+  echo xxx | umeed tx distribution withdraw-rewards $VAL_WALLET --commission --yes --gas auto --from $WALLET --chain-id umee-1 -y
   sleep 5
 
   BALANCE=$(umeed query bank balances $ADD_WALLET | grep "amount" | cut -d'"' -f 2)
-  BALANCE=$(echo $BALANCE | cut -d' ' -f 2)
-  BALANCE=$((BALANCE - 200000))
+#  BALANCE=$(echo $BALANCE | cut -d' ' -f 2)
+  BALANCE=$((BALANCE - 50000))
   sleep 5
   printf "%b\n\n\n" "--------------------------------------------------------------------------------"
-  echo xxx | umeed tx staking delegate $VAL_WALLET $BALANCE${RATE} --gas auto --from $WALLET --chain-id umee-alpha-mainnet-3 -y
-  sleep 30
+  echo xxx | umeed tx staking delegate $VAL_WALLET $BALANCE${RATE} --gas auto --from $WALLET --chain-id umee-1 -y
+  sleep 60
 done
